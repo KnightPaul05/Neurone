12bd3f57c6f11143f6c39511b8a5f3cbc2b757bc
Paul Chevalier <paul.guillaume.chevalier@gmail.com>
2025-02-11 03:16:22 +0100

    publiication

--- neuron.py
+++ neuron.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 class Neuron():
-    def __init__() :
-        a = 2
+    def __init__(self,input,biais,weights) :
+        self.input = input
+        self.output = 0
+        self.biais = biais
+        self.weights = weights
+
+
