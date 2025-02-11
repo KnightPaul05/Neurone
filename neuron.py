c154943bd4bf00f3be68e282b6dffbb1ee40bed0
Paul Chevalier <paul.guillaume.chevalier@gmail.com>
2025-02-11 19:05:44 +0100

    fin dun neurone

--- neuron.py
+++ neuron.py
@@ -1,10 +1,34 @@
 import numpy as np
 
 class Neuron():
-    def __init__(self,input,biais,weights) :
-        self.input = input
-        self.output = 0
-        self.biais = biais
-        self.weights = weights
+    def __init__(self,input_size) :
+        self.weights = np.random.rand(input_size)
+        self.bias = np.random.randn()
+        self.erreurs = []
+        self.sorties = []
+    
+    
 
+    def activation(self, x):
+        """Fonction d'activation ReLu"""
+        return x * ( x > 0)
+
+    def outputs(self,inputs):
+        '''Calcule la sortie du neurone'''
+        x = np.dot(inputs, self.weights) + self.bias
+        return self.activation(x)
+
+    def entrainement(self,inputs,target,learning_rate=0.001):
+        '''Mise à jour des poids avec descente de gradient'''
+        sortie = self.outputs(inputs)
+        self.sorties.append(sortie)
+        erreur = sortie - target
+        self.erreurs.append(erreur**2)
+        #Descente de gradiant avec la fonction de ReLu
+        gradiant = erreur*np.where(sortie > 0, 1, 0)
+
+        #Mise à jour des poids et du bias
+        self.weights -= learning_rate*gradiant*inputs
+        self.bias -= learning_rate*gradiant
+    
 
--- testes.py
+++ testes.py
@@ -1,7 +1,19 @@
 import numpy as np
+from neuron import Neuron
+import matplotlib.pyplot as plt
 
-def relutest(x):
-    return x * (x>0)
+neuron1 = Neuron(input_size=3)
+inputs = np.array([0.5,0.3,0.2])
+target = 1
 
-for i in range(5):
-    print(np.random.rand())
+
+epochs = 1000
+
+for _ in range(epochs):
+    neuron1.entrainement(inputs,target)
+
+plt.plot(neuron1.sorties)
+plt.xlabel('Itération')
+plt.ylabel('Erreur quadratique')
+plt.title('Evolution de lerreur au fil des itérations')
+plt.show()
